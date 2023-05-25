753b16c6f2b250e0830bdee7adc4e7ce7cf4a1a8
Ken McCann <ken.mccann@aquasec.com>
2023-05-25 10:36:59 +0200

    add http server for downloading any artifacts

--- aqua-iris.py
+++ aqua-iris.py
@@ -2,9 +2,12 @@ import psycopg2
 import argparse
 from os import getenv
 import sys
+from flask import Flask, send_file
 
 parser = argparse.ArgumentParser(description='Aqua Security metrics gathering tool necessary for assessing risk and security posture as seen by the Aqua Platform ')
 parser.add_argument('-s', '--server', help='PostgreSQL hostname or IP', required=True)
+parser.add_argument('-d', '--daemon', help='Run in daemon mode, starting the http server',
+                    action='store_true')
 args = parser.parse_args()
 
 # Get database password from environment
@@ -12,11 +15,16 @@ db_password = getenv('SCALOCK_DBPASSWORD')
 
 if __name__ == '__main__':
     try:
-        conn = psycopg2.connect(f"host={args.server} dbname=scalock user=postgres password={db_password}")
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM settings")
-        records = cur.fetchall()
-        print(records)
+        if args.daemon:
+            app = Flask(__name__)
+            app.run()
+        else: 
+          conn = psycopg2.connect(f"host={args.server} dbname=scalock user=postgres password={db_password}")
+          cur = conn.cursor()
+          #cur.execute("SELECT * FROM settings")
+          cur.execute(open("csp-queries/scalock/image_repo_vuln_severity_distribution.sql", "r").read())
+          records = cur.fetchall()
+          print(records)
 
     except KeyboardInterrupt:
         print("\nExiting by user request.\n", file=sys.stderr)
