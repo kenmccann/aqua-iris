14328368f822d97b87ce6577655eaccc1f51ca01
Ken McCann <ken.mccann@aquasec.com>
2023-06-22 13:57:47 +0200

    updated statistic description

--- csp-queries/scalock/image_inventory_stats.sql
+++ csp-queries/scalock/image_inventory_stats.sql
@@ -7,4 +7,4 @@ select 'Number of images' as Metric, count(DISTINCT id)::text as Value from regi
 UNION ALL
 select 'Number of images in running containers' as Metric, count(DISTINCT oci_image_id)::text as Value from containers
 UNION ALL
-select '% of images scanned but not used' as Metric, TO_CHAR((SELECT count(DISTINCT oci_image_id) from containers)/(SELECT count(DISTINCT id) from registry_images)::real * 100,'fm990D00%')
+select '% of images scanned that are in use' as Metric, TO_CHAR((SELECT count(DISTINCT oci_image_id) from containers)/(SELECT count(DISTINCT id) from registry_images)::real * 100,'fm990D00%')
