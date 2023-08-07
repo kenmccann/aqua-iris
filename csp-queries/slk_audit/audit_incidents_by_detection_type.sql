c74b37170555ea4a2aed2ca1cca89392b330f553
Tsvi Korren <tsvi@aquasec.com>
2023-08-07 03:25:51 -0400

    add columns and docker events

--- csp-queries/slk_audit/audit_incidents_by_detection_type.sql
+++ csp-queries/slk_audit/audit_incidents_by_detection_type.sql
@@ -13,12 +13,16 @@ COALESCE(data ->>'signature_name',data ->>'control') as Control,
  END as response,
 count(*) FILTER (WHERE to_timestamp(createtime) > current_date - interval '7 days' ) as  Events_7_days,
 count(*) FILTER (WHERE to_timestamp(createtime) > current_date - interval '30 days' ) as  Events_30_days,
-count(*) FILTER (WHERE to_timestamp(createtime) > current_date - interval '180 days' ) as  Events_180_days
+count(*) FILTER (WHERE to_timestamp(createtime) > current_date - interval '180 days' ) as  Events_180_days,
+max (id) as latest_id,
+max(to_timestamp(createtime)) as latest_date
 FROM public.audit
 where
   (type = 'Runtime' OR
   type = 'host.runtime' OR
-  type = 'Malware')
+  type = 'Malware' OR
+  type = 'Docker')
   AND action != 'Scan'
+  AND COALESCE(data ->>'signature_name',data ->>'control') is not null
 group by type,category, action, Control, response
 ORDER BY events_7_days DESC
