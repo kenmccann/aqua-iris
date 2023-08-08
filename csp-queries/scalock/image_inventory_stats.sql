587b3089550b20a3e955a10d87714bec393ce7f5
tsvikorren <tsvikorren@gmail.com>
2023-08-08 15:56:03 -0400

    Update image_inventory_stats.sql

--- csp-queries/scalock/image_inventory_stats.sql
+++ csp-queries/scalock/image_inventory_stats.sql
@@ -1,4 +1,6 @@
 --Image inventory stats
+select 'Number of containers' as Metric, count(DISTINCT id)::text as Value from containers where status = 'running'
+UNION ALL
 select 'Number of registries' as Metric, count(DISTINCT registry_id)::text as Value from registry_images
 UNION ALL
 select 'Number of repositories' as Metric, count(DISTINCT repository_id)::text as Value from registry_images
@@ -7,4 +9,4 @@ select 'Number of images' as Metric, count(DISTINCT id)::text as Value from regi
 UNION ALL
 select 'Number of images in running containers' as Metric, count(DISTINCT oci_image_id)::text as Value from containers
 UNION ALL
-select '% of images scanned that are in use' as Metric, TO_CHAR((SELECT count(DISTINCT oci_image_id) from containers)/(SELECT count(DISTINCT id) from registry_images)::real * 100,'fm990D00%')
+select '% of images scanned and running' as Metric, TO_CHAR((SELECT count(DISTINCT oci_image_id) from containers)/(SELECT count(DISTINCT id) from registry_images)::real * 100,'fm990D00%')
