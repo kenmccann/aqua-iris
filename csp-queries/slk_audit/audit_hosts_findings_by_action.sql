2c3b2481aa2d400c7de2ec695a774647cc01eeac
tsvikorren <tsvikorren@gmail.com>
2023-08-08 16:01:12 -0400

    Update audit_hosts_findings_by_action.sql

--- csp-queries/slk_audit/audit_hosts_findings_by_action.sql
+++ csp-queries/slk_audit/audit_hosts_findings_by_action.sql
@@ -1,10 +1,15 @@
---Host audit events by action type
 SELECT
-action,
-count(*) FILTER (WHERE to_timestamp(createtime) > current_date - interval '7 days' ) as  Events_7_days,
-count(*) FILTER (WHERE to_timestamp(createtime) > current_date - interval '30 days' ) as  Events_30_days,
-count(*) FILTER (WHERE to_timestamp(createtime) > current_date - interval '180 days' ) as  Events_180_days
-FROM audit
-WHERE type = 'host.runtime' AND category != 'behavioral' AND to_timestamp(createtime) > current_date - interval '180 days' 
-GROUP BY action
-ORDER BY events_7_days DESC
+ type,
+ category,
+ action,
+ COALESCE(data ->>'signature_name',data ->>'control') as Control,
+ CASE
+  WHEN data ->>'result' = '2' THEN 'Blocked'
+  ELSE 'Detected'
+ END as response,
+count(*) as event_count,
+max (id) as latest_id,
+max(to_timestamp(createtime)) as latest_date
+FROM public.audit
+group by type,category, action, Control, response
+ORDER BY event_count DESC
